664aa176729d4d318403028f45435bce83d45b8d
Rasa Jakimaviciute <rjakimaviciute@gmail.com>
2022-08-08 21:34:25 +0300

    date abd Time, Regex

---
+++ src/PalindromeAnswer.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PalindromeAnswer {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a name");
+        String input = scanner.nextLine().trim().toLowerCase().replaceAll(" ", "");
+        // MA DAM
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length()/2; i++){
+            if (input.charAt(i) != input.charAt(input.length()-1-i)){
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome){
+            System.out.printf("Your input %s is a palindrome \n", input);
+        }else{
+            System.out.printf("Your input %s is not a palindrome \n", input);
+        }
+
+
+    }
+}
--- src/homeWork/Palindrome9.java
+++ src/homeWork/Palindrome9.java
@@ -7,7 +7,8 @@ public class Palindrome9 {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a name");
-        String name = scanner.nextLine().trim().toLowerCase();
+        String name = scanner.nextLine().trim().toLowerCase().replaceAll(" ", "");
+        // MA DAM
         String y = "";
 
         for (int i = name.length()-1; i >= 0; i--){
@@ -30,20 +31,6 @@ public class Palindrome9 {
 
 
 
-       /* for (int i = 0; i < name.length()/2; i++) {
-            if (String [i] !== String [name.length() - 1 - i]) {
-                System.out.printf("%s is not a palindrome", name);
-            } else {
-                System.out.printf("%s is a palindrome", name);
-            }
-
-for (int i = 0; i == name.length() - 1- i; i++) {
-            if (i != name.length() - 1 - i) {
-                System.out.printf("%s is not a palindrome", name);
-            } else {
-                System.out.printf("%s is a palindrome", name);
-            }
-        }*/
 
 
 
